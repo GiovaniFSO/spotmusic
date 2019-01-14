c7e9664b3ba3154654b25f431d0fc747d7e3180c
Giovani Fernandes <giovani_oli@hotmail.com>
2019-01-13 23:41:04 -0400

    add favorite artist

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
     root to: "dashboard#index", as: :authenticated_root
     resources :search, only: [:index, :new], as: :searches
     resources :categories, only: :show
-    resources :artists, only: :show
+    resources :artists, only: :show do
+      post "/favorite", to: "favorites#create", on: :member, defaults: { format: :js, favoritable_type: 'Artist' }
+      delete "/favorite", to: "favorites#destroy", on: :member, defaults: { format: :js, favoritable_type: 'Artist' }
+    end
     resources :albums, only: :show do
       resources :recently_heards, only: :create
       post "/favorite", to: "favorites#create", on: :member, defaults: { format: :js, favoritable_type: 'Album' }
